98b0aa20f9ee384044a56f59461d64633c878ec9
Tanuja Mohan <mohantanuja@yahoo.com>
2016-01-19 17:15:33 -0800

    first commit

---
+++ hw1/tommytrojan.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  if(argc < 3){
+    cerr << "Please provide an input and output file." << endl;
+    return 1;
+  }
+  ifstream input(argv[1]);
+  ofstream output(argv[2]);
+
+  int floors;
+  int *floorsizes;
+  string ***trojans;
+  string curr;
+
+  input >> floors;
+  trojans = new string**[floors];
+
+  //you will need to keep track of how many people are on each floor.
+  floorsizes = new int[floors];
+
+  for (int i = 0; i < floors; i++) {
+	  floorsizes[i] = 0;
+	  trojans[i] = NULL;
+  }
+  while(getline(input, curr)) {
+	  stringstream ss;
+	  ss << curr;
+	  ss >> curr;
+	  if (curr == "MOVEIN") {
+		  int i,k;
+		  ss >> i;
+		  ss >> k;
+		  if (ss.fail()) {
+			  output << "Error - incorrect command" << endl;
+		  }
+		  else {
+
+		  }
+	  }
+	  else if (curr == "MOVEOUT") {
+
+	  }
+	  else if (curr == "OBTAIN") {
+
+	  }
+	  else if (curr == "OUTPUT") {
+
+	  }
+	  else {
+
+	  }
+  }
+  return 0;
+}
